4dc2ddfca0c6dcebadf7a46734f5d9743f0bfc7c
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-05-06 01:03:47 +0200

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -10,17 +10,19 @@ let rec handle client_socket =
 
 
 let () =
-  (* You can use print statements as follows for debugging, they'll be visible when running tests. *)
   Printf.eprintf "Logs from your program will appear here!\n";
 
   (* Create a TCP server socket *)
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
   bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
-  listen server_socket 2;
-
-  (* Uncomment the code below to pass the first stage *)
-  let client_socket, _ = accept server_socket in 
-    handle client_socket;
-  close client_socket;
+  listen server_socket 1;
+  
+  while true do
+    let client_socket, _ = accept server_socket in
+    let _ = Thread.create(fun () -> 
+      handle client_socket;
+      close client_socket;
+    ) () in ()
+  done;
   close server_socket
